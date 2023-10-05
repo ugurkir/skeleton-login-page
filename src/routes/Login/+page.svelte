6d38539e463500f0a6c6152d587310021c4f4745
ugur <ugurkir289@gmail.com>
2023-10-05 04:50:23 +0300

    responsive

--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -1,95 +1,98 @@
 <script>
-
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 
 	let email = '';
 	let password = '';
 	let dark = false;
-  
+
 	function handleLogin() {
-	  // Handle login logic here
-	  console.log(`Email: ${email}, Password: ${password}`);
+		console.log(`Email: ${email}, Password: ${password}`);
 	}
 
 	function darkMode() {
 		dark = !dark;
 	}
-  </script>
-  
-  
+</script>
 
-
-  <main class={dark? "dark" : "light"}>
+<main class={`${dark? "dark" : "light"} flex flex-col justify-center items-center h-screen`}>
 	
-	<h1 class={dark? "darkText" : "lightText"}>Login</h1>
-	<form class={dark? "darkForm" : "lightForm"} on:submit|preventDefault={handleLogin}>
-	  <label for="email" >Email:</label>
-	  <input id="email" type="email" bind:value={email} required />
-  
-	  <label for="password" >Password:</label>
-	  <input id="password" type="password" bind:value={password} required />
-  
-	  <button type="submit">Login</button>
-	</form>
-	<LightSwitch on:click={darkMode}/>
-  </main>
-  
+	<h1 class={`${dark? "darkText" : "lightText"} text-xl sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl`}>Login</h1>
+	<form class={`${dark? "darkForm" : "lightForm"} flex flex-col gap-4 p-8 rounded-xl shadow-md`} on:submit|preventDefault={handleLogin}>
+		<label for="email" class="text-lg">Email:</label>
+		<input id="email" type="email" bind:value={email} required class="p-2 rounded-md" />
 
-  <style>
+		<label for="password" class="text-lg">Password:</label>
+		<input id="password" type="password" bind:value={password} required class="p-2 rounded-md" />
+
+		<button type="submit" class="p-2 rounded-md bg-blue-500 text-white cursor-pointer hover:bg-blue-700">Login</button>
+	</form>
+	<p class={`${dark? "darkText" : "lightText"} text-sm sm:text-sm md:text-base lg:text-base xl:text-lg`}>Don't have an account?<a href="./Signup" class="signup-link">Signup!</a></p>
+	<LightSwitch on:click={darkMode}/>
+</main>
+
+
+<style>
+
+	.signup-link {
+		color: #007BFF;
+	}
+
 	.dark {
 		background-color: #222222;
 		color: #222222;
-		
 	}
 
 	.light {
 		background-color: #dddada;
 		color: #dddada;
 	}
+
 	.darkText{
 		color: #dddada;
 	}
+
 	.lightText{
 		color: #222222;
 	}
+
 	.darkForm {
 		background-color: #dddada;
 	}
+
 	.lightForm {
-		background-color: #222222; ;
+		background-color:#222222; ;
 	}
+
 	main {
-	  display: flex;
-	  flex-direction: column;
-	  justify-content: center;
-	  align-items: center;
-	  height: 100vh;
-	  
-		
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
 	}
-  
+
 	form {
-	  display: flex;
-	  flex-direction: column;
-	  gap: 1rem;
-	  padding: 2rem;
-	  border-radius: 1rem;
-	  box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 2rem;
+		border-radius: 1rem;
+		box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
 	}
-  
+
 	input, button {
-	  padding: 1rem;
-	  border-radius: .5rem;
+		padding: 1rem;
+		border-radius: .5rem;
 	}
-  
+
 	button {
-	  background-color: #007BFF;
-	  color: white;
-	  cursor: pointer;
+		background-color: #007BFF;
+		color: white;
+		cursor: pointer;
 	}
-  
+
 	button:hover {
-	  background-color: #0056b3;
+		background-color: #0056b3;
 	}
-  </style>
-  
+
+</style>
